d156b8a8764b72addc13aa647c604dd817f48566
Dev Ruhela <leo18jin@gmail.com>
2025-09-17 00:21:40 +0530

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,7 +9,6 @@ export interface Operation {
   scheduledTime: string;
   duration: number; 
   status: 'Scheduled' | 'In Progress' | 'Completed' | 'Cancelled';
-  
   priority: 'Low' | 'Medium' | 'High' | 'Emergency';
 
   notes?: string;
